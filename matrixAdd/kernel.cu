6eb455af7494a77e7f7093c9179b8b962af8be3d
Mattia Massarenti <mattia.mas2003@gmail.com>
2026-03-19 22:22:49 +0100

    create matrixAdd (working on it)

---
+++ matrixAdd/kernel.cu
@@ -0,0 +1,111 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+
+cudaError_t matrixAdd(int **c, const int **a, const int **b, unsigned int xDim, unsigned int yDim);
+
+__global__ void addKernel(int **c, const int **a, const int **b)
+{
+    int i = threadIdx.x;
+    c[i] = a[i] + b[i];
+}
+
+int main()
+{
+    const int xDim = 5;
+    const int yDim = 5;
+    const int a[xDim][yDim] = {
+        { 1, 1, 1, 1, 1},
+        { 1, 1, 1, 1, 1},
+        { 1, 1, 1, 1, 1},
+        { 1, 1, 1, 1, 1}
+    };
+    const int b[xDim][yDim] = {
+        { 2, 2, 2, 2, 2},
+        { 2, 2, 2, 2, 2},
+        { 2, 2, 2, 2, 2},
+        { 2, 2, 2, 2, 2}
+    };;
+    int c[xDim][yDim] = { 0 };
+
+    cudaError_t cudaStatus = matrixAdd(c, a, b, xDim, yDim);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "matrixAdd failed!");
+        return 1;
+    }
+
+    cudaStatus = cudaDeviceReset();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaDeviceReset failed!");
+        return 1;
+    }
+
+    return 0;
+}
+
+cudaError_t matrixAdd(int **c, const int **a, const int **b, unsigned int xDim, unsigned int yDim)
+{
+    int *dev_a = 0;
+    int *dev_b = 0;
+    int *dev_c = 0;
+    cudaError_t cudaStatus;
+
+    cudaStatus = cudaSetDevice(0);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
+        goto Error;
+    }
+
+    cudaStatus = cudaMalloc((void**)&dev_c, xDim * sizeof(int));
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMalloc failed!");
+        goto Error;
+    }
+
+    cudaStatus = cudaMalloc((void**)&dev_a, xDim * sizeof(int));
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMalloc failed!");
+        goto Error;
+    }
+
+    cudaStatus = cudaMalloc((void**)&dev_b, xDim * sizeof(int));
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMalloc failed!");
+        goto Error;
+    }
+
+    cudaStatus = cudaMemcpy(dev_a, a, xDim * sizeof(int), cudaMemcpyHostToDevice);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMemcpy failed!");
+        goto Error;
+    }
+
+    cudaStatus = cudaMemcpy(dev_b, b, xDim * sizeof(int), cudaMemcpyHostToDevice);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMemcpy failed!");
+        goto Error;
+    }
+
+    addKernel<<<1, xDim>>>(dev_c, dev_a, dev_b);
+
+    cudaStatus = cudaDeviceSynchronize();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
+        goto Error;
+    }
+
+    cudaStatus = cudaMemcpy(c, dev_c, xDim * sizeof(int), cudaMemcpyDeviceToHost);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMemcpy failed!");
+        goto Error;
+    }
+
+Error:
+    cudaFree(dev_c);
+    cudaFree(dev_a);
+    cudaFree(dev_b);
+    
+    return cudaStatus;
+}
